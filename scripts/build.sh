448ca67daef041293966619186cc7a3431c53981
536b656c6c79 <originalfroaz@gmail.com>
2025-05-30 10:37:44 +0000

    feat: add versioning to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+VERSION=$1
 
 echo "Building React frontend..."
 cd ../src/web
@@ -7,8 +8,15 @@ pnpm run build
 cd ..
 
 echo "Building Olingo..."
+if [ -z "${VERSION}" ]; then
+    echo "VERSION IS EMPTY OR UNSET, DEFAULTING TO LATEST COMMIT HASH AS VERSION"
+    go build -o build/olingo -buildvcs=true -ldflags "-X main.version=$(git rev-parse --short HEAD) -s -w" -buildvcs=true
+    GOOS=windows GOARCH=amd64 go build -o build/olingo.exe -buildvcs=true -ldflags "-X main.version=$(git rev-parse --short HEAD) -s -w" -buildvcs=true
+else
+    echo "VERSION SET TO $VERSION"
+    go build -o build/olingo -buildvcs=true -ldflags "-X main.version=$VERSION -s -w" -buildvcs=true
+    GOOS=windows GOARCH=amd64 go build -o build/olingo.exe -buildvcs=true -ldflags "-X main.version=$VERSION -s -w" -buildvcs=true
+fi
 
-go build -o build/olingo -buildvcs=true -ldflags "-X main.version=$(git rev-parse --short HEAD) -s -w" -buildvcs=true
-GOOS=windows GOARCH=amd64 go build -o build/olingo.exe -buildvcs=true -ldflags "-X main.version=$(git rev-parse --short HEAD) -s -w" -buildvcs=true
 
 echo "Build completed! Binary is in ./build/" 
