7ed1949588153bec33eb5ffa75848636eb504e2e
536b656c6c79 <originalfroaz@gmail.com>
2025-05-30 10:49:53 +0000

    feat: add background

---
+++ src/web/src/components/dottedBackground.tsx
@@ -0,0 +1,6 @@
+export function DottedBackground() {
+    return (
+    <div
+    className="absolute inset-0 h-full w-full bg-[radial-gradient(#5e5d5d_1px,transparent_1px)] [background-size:20px_20px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_30%,transparent_100%)] -z-20"
+    ></div>);
+}
